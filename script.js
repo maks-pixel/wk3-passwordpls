a2f163522549bca521f67ee767446f562436e242
Makda Girmay <makdagirmay4@gmail.com>
2021-09-12 20:54:36 -0400

    character number perameters

--- script.js
+++ script.js
@@ -13,3 +13,29 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//pick number of characters you want the password to be
+var charNumb = window.prompt('Choose the amount of characters you want your password to be. Must be between 8-128 characters');
+if (charNumb < 8 || charNumb > 128) {
+
+  window.alert("Invalid entry. You must enter a number between 8-128 for the amount of characters you want in your password")
+
+};
+
+//pick if you want lowercase yes or no
+
+
+//pick if you want upper case yes or no
+
+
+//pick if you want numbers yes or no
+
+
+//pick if you want special characters
+
+
+//if you didnt pick one you get angry message big no no
+
+
+//password gets generated based on input
+
+
