aafa4cdc49828987a30e6b9257283810db11f4e5
Makda Girmay <makdagirmay4@gmail.com>
2021-09-14 01:33:42 -0400

    creating a function to create password and run through writePassword

--- script.js
+++ script.js
@@ -1,59 +1,61 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-
-var lowerChar = "abcdefghijklmnopqrstuvwxyz";
-var upperChar = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
-var specialChar = "!#$%&'()*+-./:;<=>?@[\^_`{|}~";
-var numb = "0123456789";
-var passChar = "";
-//pick number of characters you want the password to be
-var charNumb = window.prompt('Choose the amount of characters you want your password to be. Must be between 8-128 characters');
-if (charNumb > 8 && charNumb < 128 ) {
-
-  //pick if you want lowercase yes or no
-  var lowerChoice = confirm("Do you want lowercase letters in your password?");
-  if (lowerChoice) {
-    passChar += lowerChar;
+    function generatePassword(){
+      var lowerChar = "abcdefghijklmnopqrstuvwxyz";
+      var upperChar = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
+      var specialChar = "!#$%&'()*+-./:;<=>?@[\^_`{|}~";
+      var numb = "0123456789";
+      var password = "";
+      var passChar = "";
+      //pick number of characters you want the password to be
+      var charNumb = window.prompt('Choose the amount of characters you want your password to be. Must be between 8-128 characters');
+      if (charNumb >= 8 && charNumb <= 128 ) {
+          //pick if you want lowercase yes or no
+          var lowerChoice = confirm("Do you want lowercase letters in your password?");
+          if (lowerChoice) {
+            passChar += lowerChar;
+          }
+      
+          //pick if you want uppercase yes or no
+          var upperChoice = confirm("wanna get wild and add uppercase letters in your password?");
+          if (upperChoice) {
+            passChar += upperChar;
+          }
+      
+          //pick if you want special character yes or no
+          var specialChoice = confirm("what about some special characters in your password?");
+          if (specialChoice) {
+            passChar += specialChar;
+          }
+        
+          //pick if you want number yes or no
+          var numbChoice = confirm("Is numbers in your password ok?");
+          if (numbChoice) {
+            passChar += numb;
+          }
+      } else {
+       //not right entry for character number
+       window.alert("Invalid entry. You must enter a number between 8-128 for the amount of characters you want in your password")
+      
+      };
+    
+      for ( i = 0; i < charNumb; i++){
+         password +=
+         passChar[Math.floor(Math.random() * passChar.length)];
+      }
+      return password;
+    }
+   
+    passwordText.value = password;
+  
   }
-
-  //pick if you want uppercase yes or no
-  var upperChoice = confirm("wanna get wild and add uppercase letters in your password?");
-  if (upperChoice) {
-    passChar += upperChar;
-  }
-
-  //pick if you want special character yes or no
-  var specialChoice = confirm("what about some special characters in your password?");
-  if (specialChoice) {
-    passChar += specialChar;
-  }
-
-
-  //pick if you want number yes or no
-  var numbChoice = confirm("Is numbers in your password ok?");
-  if (numbChoice) {
-    passChar += numb;
-  }
-
-} else {
- //not right entry for character number
-  window.alert("Invalid entry. You must enter a number between 8-128 for the amount of characters you want in your password")
-};
-
-
-
+  
